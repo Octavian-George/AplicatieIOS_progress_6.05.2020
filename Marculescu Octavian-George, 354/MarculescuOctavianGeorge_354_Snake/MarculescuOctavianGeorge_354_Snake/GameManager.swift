629a247a80dcb842fb34d6be86216ac0c24d49a8
Octavian-George <38643486+Octavian-George@users.noreply.github.com>
2020-05-06 11:50:31 +0200

    Add files via upload

---
+++ Marculescu Octavian-George, 354/MarculescuOctavianGeorge_354_Snake/MarculescuOctavianGeorge_354_Snake/GameManager.swift
@@ -0,0 +1,14 @@
+//
+//  GameManager.swift
+//  MarculescuOctavianGeorge_354_Snake
+//
+//  Created by user169232 on 5/5/20.
+//  Copyright © 2020 Marculescu Octavian. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager{
+    
+    
+}
---
+++ Marculescu Octavian-George, 354/MarculescuOctavianGeorge_354_Snake/MarculescuOctavianGeorge_354_Snake/GameScene.swift
@@ -0,0 +1,99 @@
+//
+//  GameScene.swift
+//  MarculescuOctavianGeorge_354_Snake
+//
+//  Created by user169232 on 5/5/20.
+//  Copyright © 2020 Marculescu Octavian. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    var gameLogo: SKLabelNode!
+    var gameProgrammer: SKLabelNode!
+    var bestScore: SKLabelNode!
+    
+    var playButton: SKShapeNode!
+    
+    var game: GameManager!
+   
+    
+    override func didMove(to view: SKView) {
+        meniulJocului()
+        game=GameManager()
+}
+    
+    override func update(_ currentTime: TimeInterval) {
+         //Called before each frame is rendered
+    }
+    private func meniulJocului() {
+        //Titlul proiectului
+        gameLogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
+        gameLogo.fontSize = 50
+        gameLogo.text = "Proiect Snake"
+        gameLogo.fontColor = SKColor.red
+        self.addChild(gameLogo)
+        //Numele creatorului
+        gameProgrammer = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameProgrammer.zPosition = 1
+        gameProgrammer.position = CGPoint(x: 0, y: gameLogo.position.y + 59)
+        gameProgrammer.fontSize = 35
+        gameProgrammer.text = "Marculescu Octavian-George,Grupa 354"
+        gameProgrammer.fontColor = SKColor.green
+        self.addChild(gameProgrammer)
+        //Cel mai bun scor
+        bestScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        bestScore.zPosition = 1
+        bestScore.position = CGPoint(x: 0, y: gameLogo.position.y - 50)
+        bestScore.fontSize = 40
+        bestScore.text = "Cel mai bun scor: 0"
+        bestScore.fontColor = SKColor.white
+        self.addChild(bestScore)
+        //Butonul de start
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 200)
+        playButton.fillColor = SKColor.red
+        let topCorner = CGPoint(x: -50, y: 50)
+        let bottomCorner = CGPoint(x: -50, y: -50)
+        let middle = CGPoint(x: 50, y: 0)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottomCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
+  }
+    
+    //functia de interactionare cu butonul de start
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.nodes(at: location)
+            for node in touchedNode {
+                if node.name == "play_button" {
+                    startGame()
+                }
+            }
+        }
+    }
+    
+    private func startGame(){
+        print ("Jocul a inceput")
+        gameLogo.run(SKAction.move(by: CGVector(dx: -50, dy: 600), duration: 0.5)) {
+        self.gameLogo.isHidden = true
+        }
+        gameProgrammer.run(SKAction.move(by: CGVector(dx: -50, dy: 600), duration: 0.5)) {
+        self.gameProgrammer.isHidden = true
+        }
+        playButton.run(SKAction.scale(to: 0, duration: 0.3)) {
+            self.playButton.isHidden = true
+        }
+        let bottomCorner = CGPoint(x: 0, y: (frame.size.height / -2) + 20)
+            bestScore.run(SKAction.move(to: bottomCorner, duration: 0.4))
+        }
+}
+
